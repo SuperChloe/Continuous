60e24846dd96df87e1cae4df43e9ca6bd8ef4230
Chloe Horgan <super.chloe@gmail.com>
2016-02-22 21:21:28 -0500

    Cocoapods and storyboard basics

--- Continuous/CreateViewController.swift
+++ Continuous/CreateViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CreateViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var habitField: UITextField!
+    @IBOutlet weak var numberField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        habitField.delegate = self
+        numberField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
